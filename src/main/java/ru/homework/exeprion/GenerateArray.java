528da5b673ef4e364486e6ca5e1965ce0f785a3c
oleg <olegarxo15@gmail.com>
2022-12-19 15:23:41 +0300

    +

--- src/main/java/ru/homework/exeprion/GenerateArray.java
+++ src/main/java/ru/homework/exeprion/GenerateArray.java
@@ -4,12 +4,12 @@ import java.util.Random;
 
 public class GenerateArray {
     private Random random = new Random();
-    private Object[] simvol = {0,1,2,3,4,5,6,7,8,9,null};
+    private Integer[] simvol = {0,1,2,3,4,5,6,7,8,9,null};
 
     public  Integer[] randomArray(int size) {
         Integer[] result = new Integer[size];
         for (int i = 0; i < size; i++){
-            result[i] = (Integer) this.simvol[random.nextInt(simvol.length)];
+            result[i] = this.simvol[random.nextInt(simvol.length)];
         }
         return result;
     }
